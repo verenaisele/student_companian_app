61905860ec69432b9b6788c4b5b3103dd0418af3
Verena Isele <v.j.isele@gmail.com>
2021-02-05 11:50:06 +0100

    js erstellt, buddy und team seite erstellt

---
+++ src/app.js
@@ -0,0 +1,45 @@
+//Code Buddy Seite
+const codeBuddies = [{
+    buddy1: "André Willrich", 
+    buddy2: "Emmanuel Losch"
+}
+, {
+    buddy1: "Sophie Ketterer",
+    buddy2: "Verena Isele"
+}
+, {
+    buddy1: "Mickey Mouse",
+    buddy2: "Miriam Brauer"
+}
+, {
+    buddy1: "Marlina Ketterer",
+    buddy2: "Verena Losch"
+}
+, {
+    buddy1: "Sophie Willrich",
+    buddy2: "André Isele"
+}
+]
+const teamlist1 = document.querySelector('#teamlist1')
+teamlist1.innerText = codeBuddies[0].buddy1
+
+const teamlist2 = document.querySelector('#teamlist2')
+teamlist2.innerText = codeBuddies[0].buddy2
+
+//Team Seite
+const teamMember = [["André Willrich", "Miriam Brauer", "Sophie Willrich", "Verena Isele", "Emmanuel Losch"], ["Sophie Ketterer", "Mickey Mouse", "Marlina Ketterer","Verena Losch", "André Isele"]]
+
+
+const ulTeamMembers = document.querySelector('#teamMembers')
+                          
+teamMember[0].forEach(teamMembername => {
+
+//Variante1:  
+ulTeamMembers.innerHTML += `<li>${teamMembername}</li>`
+
+//Variante2:
+//const li = document.createElement('li')
+//li.innerText = teamMembername
+//ulTeamMembers.appendChild(li)
+     })
+                            
