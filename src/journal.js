66256565ff2cd8314447396a0862f4fa087fe5ae
André Willrich <andre@willrich.info>
2021-02-12 21:16:52 +0100

    Sterne und Rechtecke werden aus der API generiert

--- src/journal.js
+++ src/journal.js
@@ -1,20 +1,20 @@
-fetch('https://muc-2020-w1-student-api.vercel.app/api/journals')
+const main = document.querySelector(".main__Journal");
 
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+fetch("https://muc-2020-w1-student-api.vercel.app/api/journals")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach(renderCard);
+  });
 
-const mottoZitat = document.querySelector('.journal__motto--zitat')
+/*const getRequest = {
+  rating: 0,
+  comprehension: 0,
+  motto: "",
+  notes: "",
+};*/
 
-const getRequest = {
-    rating: 3,
-    comprehension: 8,
-    motto: "Oh dear",
-    notes: "My coaches keep giving me Harry Potter exercises.",
-};
-
-const main = document.querySelector('.main__Journal')
-
-const card = main.innerHTML += `<section class="journal">
+function renderCard(journal) {
+  main.innerHTML += `<section class="journal">
 
       <div class="journal__container">
           <h2 class="journal__header">Yesterdays Journal:</h2>
@@ -22,40 +22,62 @@ const card = main.innerHTML += `<section class="journal">
           <div class="journal__rating mg-bt">
               <h3 class="journal__subheader">Rating:</h3>
               <div class="journal__star">
-                  <div class="journal__star--act"></div>
-                  <div class="journal__star--act"></div>
-                  <div class="journal__star--act"></div>
-                  <div class="journal__star--deact"></div>
-                  <div class="journal__star--deact"></div>
+                  ${getStars(journal.rating)}
               </div>
           </div>
 
           <div class="journal__comprehension mg-bt">
               <h3 class="journal__subheader">Comprehension:</h3>
               <div class="journal__rectangle">
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--act"></div>
-                  <div class="journal__rect--deact"></div>
-                  <div class="journal__rect--deact"></div>
-                  <div class="journal__rect--deact"></div>
+                  ${getRectangles(journal.comprehension)}
               </div>
           </div>
 
           <div class="journal__motto mg-bt">
               <h3 class="journal__subheader">Motto:</h3>
-              <p class="journal__motto--zitat">„Thats life in the city“</p>
+              <p class="journal__motto--zitat">„${journal.motto}“</p>
           </div>
           <div class="journal__notes">
               <h3 class="journal__subheader">Notes:</h3>
-              <p class="journal__notes--text">Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si
-                  mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque.</p>
+              <p class="journal__notes--text">${journal.notes}</p>
           </div>
 
       </div>
-  </section>`
+  </section>`;
+}
+
+function getStars(anzahl) {
+  let html = "";
+
+  for (let i = 0; i < anzahl; i++) {
+    html += getSymbolAct("star");
+  }
+
+  for (let i = 0; i < 5 - anzahl; i++) {
+    html += getSymbolDeAct("star");
+  }
+
+  return html;
+}
+
+function getRectangles(anzahl) {
+  let html = "";
+
+  for (let i = 0; i < anzahl; i++) {
+    html += getSymbolAct("rect");
+  }
+
+  for (let i = 0; i < 10 - anzahl; i++) {
+    html += getSymbolDeAct("rect");
+  }
+
+  return html;
+}
+
+function getSymbolAct(symbol) {
+  return `<div class="journal__${symbol}--act"></div>`;
+}
+
+function getSymbolDeAct(symbol) {
+  return `<div class="journal__${symbol}--deact"></div>`;
+}
