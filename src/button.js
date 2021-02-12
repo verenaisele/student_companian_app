186d09c3d3f9e5ffa16ce0dfac4f8f8c1c102276
Sophie Ketterer <mail@sophieketterer.de>
2021-02-12 15:23:27 +0100

    button -post

--- src/button.js
+++ src/button.js
@@ -10,11 +10,42 @@ const postRequest = {
   motto: "Oh dear",
   notes: "My coaches keep giving me Harry Potter exercises.",
 };
+
 button.addEventListener("click", () => {
   postRequest.motto = mottoInput.value;
   postRequest.notes = notesInput.value;
   console.log(postRequest);
 });
+
+
+fetch('https://muc-2020-w1-student-api.vercel.app/api/journals', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(postRequest),
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+
+
+/* post('https://muc-2020-w1-student-api.vercel.app/api/journals', postRequest)
+ .then(todo => {
+   renderTodo(todo)
+ })
+ .catch((err) => {
+   console.log(err.message)
+ })
+}
+*/
+
+
+function post(url, data) {
+  return fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+}
 
 //1. Post
 //2. Sterne * Kästchen (auf jeden Stern + Kästchen einen eventListener, jedes mal wenn Stern ist click einen LocalStorage, button abschicken - was steht drin)
